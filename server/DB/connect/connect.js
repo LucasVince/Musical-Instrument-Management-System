f531677483a61211c470eef8a6f0a2ed98a329e6
Lucas <154390549+LucasVince@users.noreply.github.com>
2024-11-28 12:34:44 -0300

    Add files via upload

--- server/DB/connect/connect.js
+++ server/DB/connect/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
     try {
-        mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@giim-music.ajbut.mongodb.net/?retryWrites=true&w=majority&appName=GIIM-Music`);
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@giim-music.ajbut.mongodb.net/?retryWrites=true&w=majority&appName=GIIM-Music`);
         console.log('connection to the server done successfully');
     } catch (err) {
         console.error(err);
